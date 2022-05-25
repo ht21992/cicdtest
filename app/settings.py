d3bf8ac9c4b4e1eeb147c7503016a61790b09e4d
ht21992 <47816410+ht21992@users.noreply.github.com>
2022-05-25 13:17:09 +0400

    shorter pass

--- app/settings.py
+++ app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ps!zg)4@6!76(yism7ctp(_vmhxuj!n-qyj@&t9o6&+pljv-v'
+SECRET_KEY = 'django-insecure-*ps!zg)4@6!76(yism7ctp(_vmhxuj!n-qyj@&t9o6&+pl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
